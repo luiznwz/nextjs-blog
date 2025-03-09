16e970ccdbcda3ef056bc2ba4a3e006f1baf0459
luiznwz <luizrenan179@gmail.com>
2025-03-08 23:07:39 -0300

    feat: new Navbar logo text, and login/sign-up buttons.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-
-
 export default function Home() {
-  return <div>Luiz</div>;
+  return <div>ljiz</div>;
 }
---
+++ components/general/Navbar.tsx
@@ -0,0 +1,42 @@
+import Link from "next/link";
+import { Button } from "../ui/button";
+
+export default function Navbar() {
+  return (
+    <nav className="py-5 flex items-center justify-between">
+      <div className="flex items-center gap-6">
+        <Link href="/">
+          <h1 className="text-3xl font-semibold">
+            Blog<span className="text-purple-500">Luuiz</span>
+          </h1>
+        </Link>
+
+        <div className="hidden sm:flex items-center gap-6">
+          <Link
+            className="text-sm font-medium hover:text-purple-500 transition-colors"
+            href="/"
+          >
+            Home
+          </Link>
+          <Link
+            className="text-sm font-medium hover:text-purple-500 transition-colors"
+            href="/dashboard"
+          >
+            Dashboard
+          </Link>
+          <Link
+            className="text-sm font-medium hover:text-purple-500 transition-colors"
+            href="/"
+          >
+            Home
+          </Link>
+        </div>
+      </div>
+
+      <div className="flex items-center gap-4">
+        <Button>Login</Button>
+        <Button variant={"secondary"}>Sing up</Button>
+      </div>
+    </nav>
+  );
+}
