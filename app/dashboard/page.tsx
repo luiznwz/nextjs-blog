53b220d114d754c2d73be23d501122fe48873f35
luiznwz <luizrenan179@gmail.com>
2025-03-15 11:38:26 -0300

    feat: redirect to register if user is not logged in

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,18 @@
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
+import { redirect } from "next/navigation"
 
-export default function DashboardRoute() {
+export default async function DashboardRoute() {
+
+  const {getUser} = getKindeServerSession();
+  const user = await getUser();
+
+  if(!user) {
+    return redirect('/api/auth/register')
+  }
+
   return (
     <div>
-      <h1>ehdskodad</h1>
+      <h1>hello from dashboard</h1>
     </div>
   )
 }
