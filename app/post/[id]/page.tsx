861a0332fac6b5ec5057aedbcb81e9b097e8ffed
luiznwz <luizrenan179@gmail.com>
2025-03-22 02:08:02 -0300

    feat: post of blog structure

--- app/post/[id]/page.tsx
+++ app/post/[id]/page.tsx
@@ -1,4 +1,7 @@
 import { prisma } from "@/app/utils/db";
+import { buttonVariants } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 
 async function getData(id: string) {
@@ -22,9 +25,45 @@ export default async function IdPage({ params }: { params: Params }) {
   const data = await getData(id);
 
   return (
-    <div>
-      <h1>{data.title}</h1>
-      <p>{data.content}</p>
+    <div className="max-w-3xl mx-auto py-8 px-4">
+      <Link
+        className={buttonVariants({ variant: "secondary" })}
+        href="/dashboard"
+      >
+        Back to Posts
+      </Link>
+
+      <div className="mb-8 mt-6">
+        <h1 className="text-3xl font-bold tracking-tight mb-4">{data.title}</h1>
+        <div className="flex items-center space-x-4">
+          <div className="flex items-center space-x-2">
+            <div className="relative size-10 overflow-hidden rounded-full">
+              <Image
+                className="object-cover"
+                src={data.authorImage}
+                alt={data.authorName}
+                fill
+              />
+            </div>
+            <p className=" font-medium">{data.authorName}</p>
+          </div>
+          <p className="text-sm text-gray-500">
+            {new Intl.DateTimeFormat("en-US", {
+              year: "numeric",
+              month: "short",
+              day: "numeric",
+            }).format(data.createdAt)}
+          </p>
+        </div>
+      </div>
+      <div className="relative h-[400px] w-full mb-8 overflow-hidden rounded-lg">
+        <Image
+          className="object-cover"
+          src={data.imageUrl}
+          alt={data.title}
+          fill
+        />
+      </div>
     </div>
   );
 }
