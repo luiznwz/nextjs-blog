6e64565e0283dc67c8fabe83e17c2b44811a9db7
luiznwz <luizrenan179@gmail.com>
2025-03-22 01:18:03 -0300

    feat: submitbutton

---
+++ components/general/Submitbutton.tsx
@@ -0,0 +1,11 @@
+import { useFormStatus } from "react-dom";
+import { Button } from "../ui/button";
+
+export function Submitbutton() {
+  const { pending } = useFormStatus();
+  return (
+    <Button className="w-fit" type="submit" disabled={pending}>
+      {pending ? "Submitting" : "Submit"}
+    </Button>
+  );
+}
